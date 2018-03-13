9892dfdc9665fe99134ecf89118d7c9eac02a4c5
Joao Paulo <joaoded@hotmail.com>
2018-03-13 16:20:57 -0300

    Inserting a todo by form

--- client/todos.js
+++ client/todos.js
@@ -5,3 +5,18 @@ Template.main.helpers({
   }
 
 });
+
+Template.main.events({
+  "submit .new-todo": (event) => {
+    const text = event.target.text.value;
+
+    Todos.insert({
+      text: text,
+      createdAt: new Date()
+    });
+
+    event.target.text.value = '';
+
+    return false;
+  }
+});
